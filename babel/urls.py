6f3c3d15636b39133504d6b46bf97a1c28403050
esandorfi <c@parleweb.fr>
2020-01-22 13:46:55 +0100

    heroku

--- babel/urls.py
+++ babel/urls.py
@@ -10,27 +10,27 @@ from django.conf.urls.static import static
 from catalog.views import home, newsroom, about, publication
 from catalog.viewscat import PublicationByDewey, PublicationDetail
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path("newsroom/", newsroom, name="newsroom"),
-        path("about/", about, name="about"),
-        path("catalog/", publication, name="publication"),
-        path(
-            "catalog/dewey_<str:deweynumber>/",
-            PublicationByDewey.as_view(),
-            name="publication-dewey",
-        ),
-        path(
-            "catalog/<int:deweynumber>_<str:authorref>_<pk>/",
-            PublicationDetail.as_view(),
-            name="publication-detail",
-        ),
-        path(
-            "catalog/<pk>/", PublicationDetail.as_view(), name="publication-detail-pk",
-        ),
-        path("", home, name="home"),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("newsroom/", newsroom, name="newsroom"),
+    path("about/", about, name="about"),
+    path("catalog/", publication, name="publication"),
+    path(
+        "catalog/dewey_<str:deweynumber>/",
+        PublicationByDewey.as_view(),
+        name="publication-dewey",
+    ),
+    path(
+        "catalog/<int:deweynumber>_<str:authorref>_<pk>/",
+        PublicationDetail.as_view(),
+        name="publication-detail",
+    ),
+    path("catalog/<pk>/", PublicationDetail.as_view(), name="publication-detail-pk",),
+    path("", home, name="home"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+print(urlpatterns)
