7966cf936c1801c0b2e3b93b44bdf94d270ac067
esandorfi <c@parleweb.fr>
2020-01-22 11:12:52 +0100

    heroku deploy

--- babel/settings.py
+++ babel/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "files_media")
 
 print(f"STATIC = {STATIC_ROOT}")
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
